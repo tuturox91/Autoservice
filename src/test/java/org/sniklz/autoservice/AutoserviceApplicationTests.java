15784bf9edea018d1677fde8fd1ddd6cefa1a770
Igor Bukur <tuturox91@gmail.com>
2023-01-17 09:19:44 +0200

    Initial commit

---
+++ src/test/java/org/sniklz/autoservice/AutoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sniklz.autoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
